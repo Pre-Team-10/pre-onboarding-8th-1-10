f0a74a8230a43d0b5eedee0bd29fb2892dacd0a5
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-12-22 16:35:32 +0900

    Feat: toastUi 생성 함수 utils 폴더에서 정의

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { Auth } from './pages';
 import theme from './styles/theme';
 import { GlobalStyle } from './styles/GlobalStyle';
@@ -7,6 +9,7 @@ import { GlobalStyle } from './styles/GlobalStyle';
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer pauseOnHover={false} />
       <ThemeProvider theme={theme}>
         <GlobalStyle />
         <Routes>
