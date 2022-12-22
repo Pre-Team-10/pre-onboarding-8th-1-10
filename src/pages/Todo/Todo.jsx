e92b6d3ac2c0afe5cf363dfb99d4b8ecafc52950
jazzyfact <jazzyfact2000@gmail.com>
2022-12-22 23:16:42 +0900

    Feat: 투두리스트 목록 추가

--- src/pages/Todo/Todo.jsx
+++ src/pages/Todo/Todo.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import TodoItem from '../../components/TodoItem';
+import {
+  CreateTodoContainer,
+  CreateTodoWrapper,
+  Title,
+  CreateTodoInput,
+  Button,
+} from './styles';
 
 function Todo() {
-  return <div>hi there</div>;
+  return (
+    <CreateTodoContainer>
+      <Title>할일 목록</Title>
+      <CreateTodoWrapper>
+        <CreateTodoInput type="text" />
+        <Button>추가</Button>
+      </CreateTodoWrapper>
+      <TodoItem />
+    </CreateTodoContainer>
+  );
 }
 
 export default Todo;
---
+++ src/pages/Todo/styles.js
@@ -0,0 +1,39 @@
+import styled, { css } from 'styled-components';
+
+export const CreateTodoContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  margin-top: 200px;
+`;
+export const CreateTodoWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: flex-start;
+  justify-content: space-around;
+`;
+export const Title = styled.h1`
+  text-align: center;
+`;
+export const CreateTodoInput = styled.input`
+  width: 400px;
+  height: 50px;
+  font-size: 15px;
+  padding: 5px;
+  border-radius: 4px;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px;
+  color: ${(props) => props.theme.color.black};
+  :focus {
+    box-shadow: 0 0 5px ${(props) => props.theme.color.black};
+  }
+`;
+export const Button = styled.button`
+  width: 50px;
+  height: 50px;
+  padding: 10px;
+  margin-bottom: 30px;
+  font-size: 15px;
+  color: white;
+  background-color: black;
+  border: none;
+  cursor: pointer;
+`;
