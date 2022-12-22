20fca3fca3ff3c02cdc98cfd6023cbfe6e1cc7ea
ddaisylee <elliejay9988@gmail.com>
2022-12-22 10:29:05 +0900

    Feat: Auth 페이지 생성

---
+++ src/pages/Auth/index.jsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as S from './styles';
+import { Button, Input } from '../../components';
+import { useValidationCheck } from '../../hooks/useValidationCheck';
+import {
+  emailValidationCheck,
+  passwordValidationCheck,
+} from '../../utils/validationCheck';
+import { getAccessToken } from '../../utils';
+import { auth } from '../../apis/auth';
+
+function Auth() {
+  const [isSignInPage, setIsSignInPage] = useState(true);
+  const navigate = useNavigate();
+  const [email, isEmailValid, handleEmailChange] =
+    useValidationCheck(emailValidationCheck);
+
+  const [password, isPasswordValid, handlePasswordChange] = useValidationCheck(
+    passwordValidationCheck,
+  );
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    auth({
+      email,
+      password,
+      isSignInPage,
+      onSuccess: () => window.location.replace('/todo'),
+    });
+  };
+
+  useEffect(() => {
+    const accessToken = getAccessToken();
+    if (accessToken) navigate('/todo');
+    if (!accessToken) navigate('/');
+  }, [navigate]);
+
+  return (
+    <S.Container>
+      <S.LeftBox src="/Img/pre-onboarding.png" />
+      <S.RightBox>
+        <S.Title>{isSignInPage ? 'Sign In' : 'Sign Up'}</S.Title>
+        <S.Form onSubmit={(e) => handleSubmit(e)}>
+          <Input
+            type="Email"
+            isValid={isEmailValid}
+            value={email}
+            message="올바른 이메일 형식을 입력해주세요."
+            onChange={(e) => handleEmailChange(e)}
+          />
+          <Input
+            type="Password"
+            isValid={isPasswordValid}
+            value={password}
+            message="비밀번호는 8자 이상입니다."
+            onChange={(e) => handlePasswordChange(e)}
+          />
+          <Button disabled={!isEmailValid || !isPasswordValid}>
+            {isSignInPage ? 'Sign In' : 'Sign Up'}
+          </Button>
+          <S.Link onClick={() => setIsSignInPage(!isSignInPage)}>
+            {isSignInPage ? 'Sign Up' : 'Sign In'}
+          </S.Link>
+        </S.Form>
+      </S.RightBox>
+    </S.Container>
+  );
+}
+
+export default Auth;
