456e5e300fa4ea244263a6f233d9029e424e59dd
jazzyfact <jazzyfact2000@gmail.com>
2022-12-23 13:08:52 +0900

    Feat: 투두리스트 수정, 삭제 추가

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,21 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TodoItemWrapper,
   TodoItemInput,
   TodoItemCheckbox,
   Button,
   DeleteButton,
+  Text,
 } from './styles';
+import { getAccessToken, showErrorToast } from '../../utils';
+import { deleteTodoApi, updateTodoApi } from '../../apis/todo';
 
-function TodoItem() {
-  return (
+function TodoItem({ id, todo, isCompleted, setIsDeleted }) {
+  const [editTodoInput, setEditTodoInput] = useState(todo);
+  const [isChecked, setIsChecked] = useState(isCompleted);
+  const [isEditTodo, setIsEditTodo] = useState(false);
+
+  const onEditTodoHandler = () => {
+    setIsEditTodo(true);
+  };
+  const onCancelHandler = () => {
+    setIsEditTodo(false);
+  };
+
+  const onUpdateTodoHandler = (e) => {
+    e.preventDefault();
+    const updateDoto = async () => {
+      try {
+        const response = await updateTodoApi(
+          id,
+          { todo: editTodoInput, isCompleted: isChecked },
+          getAccessToken(),
+        );
+        setIsEditTodo(false);
+        setIsDeleted(true);
+      } catch (e) {
+        showErrorToast(e.response.data.message);
+        console.log(e.response);
+      }
+    };
+    updateDoto();
+  };
+
+  const onDeletehandler = (e) => {
+    e.preventDefault();
+    const deleteTodo = async () => {
+      try {
+        if (getAccessToken()) {
+          const response = await deleteTodoApi(id, getAccessToken());
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    deleteTodo();
+  };
+
+  const checkboxHandler = (e) => {
+    setIsChecked(e.target.checked);
+  };
+
+  return isEditTodo ? (
     <TodoItemWrapper>
       <TodoItemCheckbox type="checkbox" />
-      <TodoItemInput type="text" />
-      <Button type="button">작성</Button>
-      <DeleteButton type="button">취소</DeleteButton>
+      <TodoItemInput
+        type="text"
+        value={editTodoInput}
+        onChange={(e) => {
+          setEditTodoInput(e.target.value);
+        }}
+      />
+      <Button type="button" onClick={onUpdateTodoHandler}>
+        완료
+      </Button>
+      <DeleteButton type="button" onClick={onCancelHandler}>
+        취소
+      </DeleteButton>
+    </TodoItemWrapper>
+  ) : (
+    <TodoItemWrapper>
+      <TodoItemCheckbox
+        type="checkbox"
+        onChange={checkboxHandler}
+        defaultChecked={isChecked}
+      />
+      <Text>{editTodoInput}</Text>
+      <Button type="button" onClick={onEditTodoHandler}>
+        수정
+      </Button>
+      <DeleteButton type="button" onClick={onDeletehandler}>
+        삭제
+      </DeleteButton>
     </TodoItemWrapper>
   );
 }
 
-export default TodoItem;
+export default React.memo(TodoItem);
--- src/components/TodoItem/styles.js
+++ src/components/TodoItem/styles.js
@@ -1,4 +1,4 @@
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 export const TodoItemWrapper = styled.div`
   display: flex;
@@ -9,7 +9,7 @@ export const TodoItemWrapper = styled.div`
 export const TodoItemInput = styled.input`
   width: 300px;
   height: 50px;
-  font-size: 15px;
+  font-size: ${(props) => props.theme.fontSize.middle};
   margin-right: 10px;
   border-radius: 4px;
   box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px;
@@ -20,27 +20,22 @@ export const TodoItemInput = styled.input`
 `;
 export const Text = styled.div`
   flex: 1;
-  font-size: 21px;
+  font-size: ${(props) => props.theme.fontSize.middle};
   width: 300px;
-  color: #495057;
-  ${(props) =>
-    props.done &&
-    css`
-      color: #ced4da;
-      text-decoration: line-through;
-    `}
+  color: ${(props) => props.theme.color.black};
 `;
 export const TodoItemCheckbox = styled.input`
   width: 30px;
   height: 50px;
   margin-right: 10px;
+  accent-color: ${(props) => props.theme.color.primary};
 `;
 export const Button = styled.button`
   width: 50px;
   height: 50px;
   padding: 5px;
   margin: 3px;
-  font-size: 15px;
+  font-size: ${(props) => props.theme.fontSize.small};
   color: white;
   background-color: ${(props) => props.theme.color.black};
   border: none;
@@ -51,9 +46,9 @@ export const DeleteButton = styled.button`
   height: 50px;
   padding: 5px;
   margin: 3px;
-  font-size: 15px;
-  color: white;
-  background-color: #ed1212;
+  font-size: ${(props) => props.theme.fontSize.small};
+  color: ${(props) => props.theme.color.white};
+  background-color: ${(props) => props.theme.color.error};
   border: none;
   cursor: pointer;
 `;
