7fb902930b0332acb34d7f1a575294e1a051b03c
jdy8739 <83811826+jdy8739@users.noreply.github.com>
2022-12-23 19:56:56 +0900

    Refactor: 중복 코드 제거 및 서버 상태, 메세지 하나의 객체로 관리 (#8)
    * Feat: 스타일제외 모든 기능 구현 완료 (#6)
    
    * Feat: 유효하지 않은 토큰 401에러 처리하는 함수 추가 및 해당 케이스 시 토큰을 제거하고 메인페이지로 리다이렉션하는 기능 구현
    
    * Feat: todo isComplete 변경 기능 추가
    
    * Fix: console.log제거
    
    * Refactor: 전에 내가 작성했던 쓰지 않은 jsx 파일 삭제
    
    * Refactor: 정규표현식으로 토큰 검사 및 에러 핸들링 추가
    
    * Create node.js.yml (#7)
    
    * Update node.js.yml
    
    * Refactor: 응답 메세지, 상태 코드 하나의 객체 안에 넣어서 저장 및 활용
    
    Co-authored-by: JAEWON HWANG <89454575+JaeWonyH@users.noreply.github.com>

--- src/utils/instance.js
+++ src/utils/instance.js
@@ -6,6 +6,18 @@ const BASE_URL = 'https://pre-onboarding-selection-task.shop';
 
 const TOKEN_REGEX = /[^0-9A-Za-z._-\s]/gi;
 
+export const statusMessageObj = {
+  idAndPwNotCorrectComment: '입력하신 이메일과 비밀번호를 확인해주세요',
+  serverProblemComment: '서버에 문제가 있습니다!',
+  tokenProblemComment: '토큰이 유효하지 않습니다! 재로그인해주세요!',
+  notValidTokenComment: '올바른 토큰값이 아닙니다.',
+};
+
+export const serverStatus = {
+  noContent: 204,
+  unauthorized: 401,
+};
+
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
 });
@@ -24,7 +36,7 @@ axiosInstanceWithToken.interceptors.request.use(
         return reqConfigWithToken;
       }
     }
-    showErrorToast('올바른 토큰값이 아닙니다.');
+    showErrorToast(statusMessageObj.notValidTokenComment);
     return new AxiosError();
   },
   (error) => {
